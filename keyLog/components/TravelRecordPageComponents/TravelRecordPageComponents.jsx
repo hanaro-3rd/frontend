444b94d611a19f23b630fb57bc4e30973e21e9c9
jordizzin <yejin328@naver.com>
2023-07-24 16:58:08 +0900

    feature:Mainpage View 구현 및 assets(png files) upload

---
+++ keyLog/components/TravelRecordPageComponents/TravelRecordPageComponents.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const TravelRecordPageComponents = () => {
+  return (
+    <View>
+      <Text>TravelRecordPageComponents</Text>
+    </View>
+  );
+};
+
+export default TravelRecordPageComponents;
+
+const styles = StyleSheet.create({});
