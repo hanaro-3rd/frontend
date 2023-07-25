254eab064dcb3fbea37ad7cd419c464775428dfc
juseong <jjs7280@gmail.com>
2023-07-25 14:10:10 +0900

    Chore: merge 충돌 해결

--- keyLog/App.tsx
+++ keyLog/App.tsx
@@ -1,6 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import PaymentPageInputComponent from './components/PaymentPageComponents/PaymentPageInputComponent';
+import TravelRecordDetailComponent from './components/TravelRecordPageComponents/TravelRecordDetailComponent';
+import TravelRecordMainComponent from './components/TravelRecordPageComponents/TravelRecordMainComponent';
 import AccountConnectPage from './pages/AccountConnectPage';
 import ExchangePage from './pages/ExchangePage';
 import MainPage from './pages/MainPage';
@@ -10,16 +13,14 @@ import LoginPatternPage from './pages/SignUp/LoginPatternPage';
 import SignUpPage from './pages/SignUp/SignUpPage';
 import TravelBudgetPage from './pages/TravelBudgetPage';
 import TravelRecordPage from './pages/TravelRecordPage';
-import TravelRecordMainComponent from './components/TravelRecordPageComponents/TravelRecordMainComponent';
-import TravelRecordDetailComponent from './components/TravelRecordPageComponents/TravelRecordDetailComponent';
-import PaymentPageInputComponent from './components/PaymentPageComponents/PaymentPageInputComponent';
+
 const App = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="MainPage">
-      <Stack.Screen
-          name="MainPage"
+      <Stack.Navigator initialRouteName='MainPage'>
+        <Stack.Screen
+          name='MainPage'
           component={MainPage}
           options={{ headerShown: false }}
         />
@@ -27,7 +28,7 @@ const App = () => {
           name='AccountConnectPage'
           component={AccountConnectPage}
         />
-      <Stack.Screen name='ExchangePage' component={ExchangePage} />
+        <Stack.Screen name='ExchangePage' component={ExchangePage} />
         <Stack.Screen name='PickUpKeyPage' component={PickUpKeyPage} />
         <Stack.Screen
           name='SignUpPage'
@@ -44,6 +45,19 @@ const App = () => {
           component={LoginPatternPage}
           options={{ headerShown: false }}
         />
+        <Stack.Screen
+          name='TravelRecordMainComponent'
+          component={TravelRecordMainComponent}
+        />
+        <Stack.Screen
+          name='TravelRecordDetailComponent'
+          component={TravelRecordDetailComponent}
+        />
+        <Stack.Screen
+          name='PaymentPageInputComponent'
+          component={PaymentPageInputComponent}
+        />
+        <Stack.Screen name='TravelRecordPage' component={TravelRecordPage} />
         <Stack.Screen name='TravelBudgetPage' component={TravelBudgetPage} />
         <Stack.Screen name='TravelRecordPage' component={TravelRecordPage} />
       </Stack.Navigator>
