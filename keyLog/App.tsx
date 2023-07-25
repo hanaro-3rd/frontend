f0ca81c072c041b52c959429a7cd5f25330e91c4
eternalclash <suchasucha@naver.com>
2023-07-25 09:20:15 +0900

    Feat: 결제내역 페이지 형성

--- keyLog/App.tsx
+++ keyLog/App.tsx
@@ -8,11 +8,15 @@ import PickUpKeyPage from './pages/PickUpKeyPage';
 import SignUpPage from './pages/SignUpPage';
 import TravelBudgetPage from './pages/TravelBudgetPage';
 import TravelRecordPage from './pages/TravelRecordPage';
+import TravelRecordMainComponent from './components/TravelRecordPageComponents/TravelRecordMainComponent';
+import TravelRecordPageDetailComponent from './components/TravelRecordPageComponents/TravelRecordDetailComponent';
+import TravelRecordDetailComponent from './components/TravelRecordPageComponents/TravelRecordDetailComponent';
+import PaymentPageInputComponent from './components/PaymentPageComponents/PaymentPageInputComponent';
 const App = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="AccountConnectPage">
+      <Stack.Navigator initialRouteName="PaymentPageInputComponent" screenOptions={{headerShown:false}}>
         <Stack.Screen name="MainPage" component={MainPage} />
         <Stack.Screen
           name="AccountConnectPage"
@@ -21,7 +25,9 @@ const App = () => {
         <Stack.Screen name="ExchangePage" component={ExchangePage} />
         <Stack.Screen name="PickUpKeyPage" component={PickUpKeyPage} />
         <Stack.Screen name="SignUpPage" component={SignUpPage} />
-        <Stack.Screen name="TravelBudgetPage" component={TravelBudgetPage} />
+        <Stack.Screen name="TravelRecordMainComponent" component={TravelRecordMainComponent} />
+        <Stack.Screen name="TravelRecordDetailComponent" component={TravelRecordDetailComponent} />
+        <Stack.Screen name="PaymentPageInputComponent" component = {PaymentPageInputComponent} />
         <Stack.Screen name="TravelRecordPage" component={TravelRecordPage} />
       </Stack.Navigator>
     </NavigationContainer>
--- keyLog/pages/AccountConnectPage.jsx
+++ keyLog/pages/AccountConnectPage.jsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const AccountConnectPage = () => {
   return (
-    <View>
-      <Text>AccountConnectPage</Text>
-    </View>
+    <SafeAreaView>
+    <Text>안녕ㅌㅌㅌㅌㅌㅌㅌㅌ</Text>
+    </SafeAreaView>
   )
 }
 
