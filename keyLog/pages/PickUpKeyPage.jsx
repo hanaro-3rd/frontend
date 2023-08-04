2b32c54b93bfc102da212b40fc231b2547961ffd
YEJIN SEO <yejin328@naver.com>
2023-08-04 13:20:33 +0900

    feat:pickUp google map api

--- keyLog/pages/PickUpKeyPage.jsx
+++ keyLog/pages/PickUpKeyPage.jsx
@@ -12,9 +12,11 @@ import styled from "styled-components/native";
 import arrow_back from "../assets/travelBudget/arrow_back.png";
 import search from "../assets/PickUp/Search.png";
 import { useNavigation } from "@react-navigation/native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import Map from "../components/PickUpKeyPageComponents/MapComponent";
 
 const Header = styled.View`
-  align-items: center;
+  align-items: yarcenter;
   width: 100%;
   height: ${heightPercentage(50)}px;
   gap: 10px;
@@ -27,7 +29,7 @@ const SearchInput = styled.View`
   display: flex;
   height: ${heightPercentage(40)}px;
   width: ${widthPercentage(332)}px;
-  padding: ${heightPercentage(8)}px ${widthPercentage(15)}px;
+  padding: ${heightPercentage(11.25)}px ${widthPercentage(15)}px;
   align-items: flex-start;
   gap: 10px;
   flex: 1 0 0;
@@ -47,17 +49,28 @@ const PickUpKeyPage = ({ navigation }) => {
   };
 
   return (
-    <View>
-      <Header>
-        <TouchableOpacity onPress={handleGoBackToPrevPage}>
-          <Image source={arrow_back} />
-        </TouchableOpacity>
-        <SearchInput>
-          <Image source={search} />
-        </SearchInput>
-      </Header>
-      <Text>PickUpKeyPage</Text>
-    </View>
+    // <View>
+    //   <Header>
+    //     <TouchableOpacity onPress={handleGoBackToPrevPage}>
+    //       <Image source={arrow_back} />
+    //     </TouchableOpacity>
+    //     <SearchInput>
+    //       <Image source={search} />
+    //     </SearchInput>
+    //   </Header>
+
+    <MapView
+      style={{ flex: 1 }}
+      provider={PROVIDER_GOOGLE}
+      region={{
+        latitude: 37.541,
+        longitude: 80.986,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+
+    // </View>
   );
 };
 
