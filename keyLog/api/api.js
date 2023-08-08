846355e7489165370f6cd333ad4cd542763245d0
juseong <jjs7280@gmail.com>
2023-08-08 15:41:56 +0900

    Feat: 비밀번호 로그인 추가

--- keyLog/api/api.js
+++ keyLog/api/api.js
@@ -1,33 +1,34 @@
-import { axiosClient } from "./axiosClient";
+import { axiosClient } from './axiosClient';
 
-export const postSignup = (signupData) => {
-  return axiosClient.post("/signup", signupData);
+export const postSignup = signupData => {
+  return axiosClient.post('/signup', signupData);
 };
 
-export const postSigninPattern = (signInPatternData) => {
-  return axiosClient.post("/signin/pattern", signInPatternData);
+export const postSigninPattern = signInPatternData => {
+  return axiosClient.post('/signin/pattern', signInPatternData);
 };
 
-export const postSigninPassword = (signInPasswordData) => {
-  return axiosClient.post("/signin/password", signInPasswordData);
+export const postSigninPassword = signInPasswordData => {
+  return axiosClient.post('/signin/password', signInPasswordData);
 };
 
-export const postVerification = (verificationData) => {
-  return axiosClient.post("/verification", verificationData);
+export const postVerification = verificationData => {
+  return axiosClient.post('/verification', verificationData);
 };
 
-export const postVerificationAuth = (verifiacationAuthData) => {
-  return axiosClient.post("/verification/auth", verifiacationAuthData);
+export const postVerificationAuth = verifiacationAuthData => {
+  return axiosClient.post('/verification/auth', verifiacationAuthData);
 };
-export const getRegistrationDeviceId = (deviceId) => {
+
+export const getRegistrationDeviceId = deviceId => {
   return axiosClient.get(`/registration/${deviceId}`);
 };
 
 export const getMarkers = () => {
-  return axiosClient.get('/marker')
-} 
+  return axiosClient.get('/marker');
+};
 
-export const postMarkers = ({markerId,markerData}) => {
-  console.log(markerId,markerData)
-  return axiosClient.post(`/marker/${markerId}`,markerData)
-}
+export const postMarkers = ({ markerId, markerData }) => {
+  console.log(markerId, markerData);
+  return axiosClient.post(`/marker/${markerId}`, markerData);
+};
