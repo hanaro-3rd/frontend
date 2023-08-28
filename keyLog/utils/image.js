26d2ba98692f6170685de52a8def993c6f7f5b61
hyunjoo <79887655+hyunjoolee201910828@users.noreply.github.com>
2023-08-28 16:58:07 +0900

    Upate: 이미지 모두 추가

--- keyLog/utils/image.js
+++ keyLog/utils/image.js
@@ -10,3 +10,84 @@ export const markerCulture =
   "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/marker-culture.png";
 export const markerEtc =
   "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/marker-etc.png";
+
+export const usaFlag =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/usa-flag.png";
+export const euFlag =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/eu-flag.png";
+export const japanFlag =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/japan-flag.png";
+export const koreaFlag =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/korea-flag.png";
+
+export const start =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/star.png";
+export const starHalf =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/star-half.png";
+
+export const keyCircle =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/key-cicle.png";
+export const placeCircle =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/place-circle.png";
+export const loop =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/loop.png";
+export const clear =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/clear.png";
+export const notifications =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/notifications.png";
+export const check =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/check.png";
+export const arrowBack =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/arrow-back.png";
+export const expand =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/expand.png";
+export const close =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/close.png";
+export const deleteIcon =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/delete.png";
+export const markerBlue =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/marker-blue.png";
+export const markderGray =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/marker-gray.png";
+export const reduce =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/reduce.png";
+
+export const paymentFail =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/payment-fail.png";
+export const paymentSuccess =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/payment-success.png";
+
+export const Fail =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/fail.png";
+export const connectSuccess =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/connect-success.png";
+export const exchangeSucess =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/exchange-success.png";
+export const serverError =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/server-error.png";
+export const clientError =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/client-error.png";
+
+export const markerMenu =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/marker-menu.png";
+export const planMenu =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/plan-menu.png";
+export const keymoneyBill =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/keymoney-bill.png";
+export const accountMenu =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/account-menu.png";
+export const keymoneyCoin =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/keymoney-coin.png";
+
+export const exchangeImage =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/exchange.png";
+export const categoryShoppging =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/category-shopping.png";
+export const categoryFood =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/category-food.png";
+export const categoryHotel =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/category-hotel.png";
+export const categoryCulture =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/category-culture.png";
+export const categoryEtc =
+  "https://keylog-bucket.s3.ap-northeast-2.amazonaws.com/category-etc.png";
